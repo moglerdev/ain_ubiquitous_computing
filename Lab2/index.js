2147991b36aeff19ab2f4b15801a8dc7594e7895
Christopher Mogler <christopher@mogler.dev>
2024-11-28 01:13:25 +0100

    Lab2: create certificate with all known hostnames and ips

--- Lab2/index.js
+++ Lab2/index.js
@@ -1,16 +1,34 @@
 const selfsigned = require("selfsigned");
+const os = require("os");
 
 const fs = require("fs/promises");
 
 // Certificate options
 const attrs = [{ name: "commonName", value: "localhost" }];
 
+// Function to get all local IP addresses
+function getHostnamesAndIPs() {
+    const interfaces = os.networkInterfaces();
+    const altNames = [
+        { type: 2, value: "localhost" },  // DNS name
+        { type: 7, ip: "127.0.0.1" },     // IP address
+    ]; // Add localhost by default
+
+    for (const iface of Object.values(interfaces)) {
+        for (const config of iface) {
+            if (config.family === "IPv4" && !config.internal) {
+                altNames.push({ type: 7, ip: config.address }); // Add IP address
+            }
+        }
+    }
+
+    return altNames;
+}
+
+
 const options = {
     days: 365, // Validity
-    altNames: [
-    { type: 2, value: "localhost" },  // DNS name
-    { type: 7, ip: "127.0.0.1" },     // IP address
-    ],
+    altNames: getHostnamesAndIPs(), // Subject Alternative Names
 };
 
 function createCertificate() {
